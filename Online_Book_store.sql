350ab9eb6677c3e3320bac4217ea3e7220984d60
Rishav Nirwan <rishavnirwan43593@gmail.com>
2025-04-14 00:33:37 -1200

    Update Online_Book_store.sql

--- Online_Book_store.sql
+++ Online_Book_store.sql
@@ -1,3 +1,56 @@
+-- Create Database
+CREATE DATABASE OnlineBookstore;
+
+
+-- Create Tables
+DROP TABLE IF EXISTS Books;
+CREATE TABLE Books (
+    Book_ID SERIAL PRIMARY KEY,
+    Title VARCHAR(100),
+    Author VARCHAR(100),
+    Genre VARCHAR(50),
+    Published_Year INT,
+    Price NUMERIC(10, 2),
+    Stock INT
+);
+DROP TABLE IF EXISTS customers;
+CREATE TABLE Customers (
+    Customer_ID SERIAL PRIMARY KEY,
+    Name VARCHAR(100),
+    Email VARCHAR(100),
+    Phone VARCHAR(15),
+    City VARCHAR(50),
+    Country VARCHAR(150)
+);
+DROP TABLE IF EXISTS orders;
+CREATE TABLE Orders (
+    Order_ID SERIAL PRIMARY KEY,
+    Customer_ID INT REFERENCES Customers(Customer_ID),
+    Book_ID INT REFERENCES Books(Book_ID),
+    Order_Date DATE,
+    Quantity INT,
+    Total_Amount NUMERIC(10, 2)
+);
+
+SELECT * FROM Books;
+SELECT * FROM Customers;
+SELECT * FROM Orders;
+
+-- Import Data into Books Table
+COPY Books(Book_ID, Title, Author, Genre, Published_Year, Price, Stock) 
+FROM 'D:\Course Updates\30 Day Series\SQL\CSV\Books.csv' 
+WITH (FORMAT csv, HEADER true);
+
+-- Import Data into Customers Table
+COPY Customers(Customer_ID, Name, Email, Phone, City, Country) 
+FROM 'D:\Course Updates\30 Day Series\SQL\CSV\Customers.csv' 
+WITH (FORMAT csv, HEADER true);
+
+-- Import Data into Orders Table
+COPY Orders(Order_ID, Customer_ID, Book_ID, Order_Date, Quantity, Total_Amount) 
+FROM 'D:\Course Updates\30 Day Series\SQL\CSV\Orders.csv' 
+WITH (FORMAT csv, HEADER true);
+
 1) Retrieve all books in the "Fiction" genre:
 select * from books
 where genre='Fiction'
